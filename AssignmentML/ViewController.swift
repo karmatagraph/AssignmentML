f9609f6a21419de1aeb21567d0b28dc3832c3d15
karma <karmatashi954@gmail.com>
2022-03-21 10:21:57 +0545

    camera setup using AVKit

--- AssignmentML/ViewController.swift
+++ AssignmentML/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        // camera setup using AVKit
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
     }
 
 
