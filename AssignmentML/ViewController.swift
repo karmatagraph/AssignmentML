cbeb91622998e9ab8be9bd3ea9345c2bcf2f27b0
karma <karmatashi954@gmail.com>
2022-03-21 11:11:51 +0545

    add of model and capture frame monitor

--- AssignmentML/ViewController.swift
+++ AssignmentML/ViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +31,35 @@ class ViewController: UIViewController {
         // we need to set the frame of the preview layer
         previewLayer.frame = view.frame
         
+        // monitoring every frame captured by the camera
+        let dataOutput = AVCaptureVideoDataOutput()
+        captureSession.addOutput(dataOutput)
+        // monitor the frame
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
     }
+        
+        
+        
 
+    // delegate method that notifies when a frame is captured
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+//        print("camera was able to capture a frame: \(Date())")
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        
+        //config for model
+        let defaultConfig = MLModelConfiguration()
+        // create model
+        guard let model = try? VNCoreMLModel(for: SqueezeNet(configuration: defaultConfig).model) else{return}
+        
+        let request = VNCoreMLRequest(model: model){
+            (finishedReq , error) in
+            // check the error and print result
+            print(finishedReq.results)
+        }
 
+        // this is responsible for analysing the image
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
 }
 
