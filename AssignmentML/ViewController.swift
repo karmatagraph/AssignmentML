1c957d52304546bee9408d9d385398f6f4cc1d7c
karma <karmatashi954@gmail.com>
2022-03-21 10:30:12 +0545

    add access to camera add of show the preview to the view

--- AssignmentML/ViewController.swift
+++ AssignmentML/ViewController.swift
@@ -20,10 +20,15 @@ class ViewController: UIViewController {
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
         
-        
         captureSession.addInput(input)
-        
         captureSession.startRunning()
+        
+        // to show the captured video on the view
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        
+        // we need to set the frame of the preview layer
+        previewLayer.frame = view.frame
         
     }
 
